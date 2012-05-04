2b82be901feb018b6f36877a5b1ca34371d0b3eb
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2012-05-04 14:37:58 +0200

    Introduce extra_fieldsets for admin

--- hub/core/admin.py
+++ hub/core/admin.py
@@ -4,10 +4,29 @@ from django.db.models import AutoField
 from django.forms import ValidationError
 from django.shortcuts import redirect
 from django.utils.translation import ugettext_lazy as _
+from copy import deepcopy
 
 from hub.core.models import CONTENT_STATUS_PUBLISHED
 
-class DisplayableAdmin(admin.ModelAdmin):
+class CoreAdmin(admin.ModelAdmin):
+    extra_fieldsets = None
+
+    def __init__(self, *args, **kwargs):
+        """
+        For ``Page`` subclasses that are registered with an Admin class
+        that doesn't implement fieldsets, add any extra model fields
+        to this instance's fieldsets. This mimics Django's behaviour of
+        adding all model fields when no fieldsets are defined on the
+        Admin class.
+        """
+        super(CoreAdmin, self).__init__(*args, **kwargs)
+        print self.extra_fieldsets
+        if self.extra_fieldsets is not None:
+            print "yo"
+            self.fieldsets = deepcopy(self.fieldsets) + self.extra_fieldsets
+
+
+class DisplayableAdmin(CoreAdmin):
     """
     Admin class for subclasses of the abstract ``Displayable`` model.
     """
---
+++ hub/utils/admin.py
@@ -0,0 +1,4 @@
+from copy import deepcopy
+
+def merge_fieldsets(fieldset1, fieldset2):
+    self.fieldsets = deepcopy(fieldset1) + fieldset2
