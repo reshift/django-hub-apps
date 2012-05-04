6ac37b25eb93168933723b98d16739bbb30aed04
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2012-05-04 12:44:27 +0200

    Adding node app as base for other content types

---
+++ hub/node/models.py
@@ -0,0 +1,33 @@
+from django.core.urlresolvers import resolve, reverse
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from datetime import *
+
+from hub.core.models import Displayable, RichText
+
+from autoslug import AutoSlugField
+from taggit.managers import TaggableManager
+
+class Node(Displayable):
+  title      = models.CharField(max_length=255)
+  slug       = AutoSlugField(populate_from='title', max_length=255, editable=True, blank=True, unique=True)
+  created_at = models.DateTimeField(auto_now_add=True, default=datetime.now())
+  updated_at = models.DateTimeField(auto_now=True)
+
+  # Taggit
+  tags = TaggableManager()
+
+  def __unicode__(self):
+    return self.title
+
+  # Grapelli autocomplete label
+  def related_label(self):
+    return u"%s" % (self.title)
+
+  @staticmethod
+  def autocomplete_search_fields():
+    return ("id__iexact", "title__icontains",)
+  
+  class Meta:
+    abstract = True
+    ordering = ('title', )
