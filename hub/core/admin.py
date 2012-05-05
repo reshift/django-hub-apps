7816f50695c21166d925b2cba1cbb7dfd98944a5
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2012-05-05 15:41:09 +0200

    Recursive run through fields

--- hub/core/admin.py
+++ hub/core/admin.py
@@ -19,7 +19,7 @@ class CoreAdmin(admin.ModelAdmin):
         self.fieldsets = deepcopy(self.fieldsets)
         for field in reversed(self.model._meta.fields):
             #if field not in Page._meta.fields and field.name != "page_ptr":
-            if getattr(instance, field.name) is not None:
+            if getattr(field, field.name) is not None:
                 self.fieldsets[0][1]["fields"].insert(3, field.name)
 
 class DisplayableAdmin(CoreAdmin):
